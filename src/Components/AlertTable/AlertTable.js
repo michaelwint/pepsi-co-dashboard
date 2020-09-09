ede0cb250f791fbb486a38e848ae40bbfe73579e
michaelwint <70092068+michaelwint@users.noreply.github.com>
2020-09-09 10:15:24 +0300

    Implemented style on AlertTable

--- src/Components/AlertTable/AlertTable.js
+++ src/Components/AlertTable/AlertTable.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Table from 'react-bootstrap/Table'
 import BootstrapTable from 'react-bootstrap-table-next';
 import filterFactory, { textFilter, numberFilter, dateFilter } from 'react-bootstrap-table2-filter';
 import './AlertTable.css'
@@ -9,15 +8,19 @@ export default function AlertTable(props) {
         { id: 1, valveGroup: "V3301", measure: 26.5, date: "2020-04-16 12:00:00"},
         { id: 2, valveGroup: "V3401", measure: 24.2, date: "2020-04-16 12:30:00"},
         { id: 3, valveGroup: "V3401", measure: 33.3, date: "2020-04-16 13:00:00"},
-        { id: 4, valveGroup: "V3301", measure: undefined, date: "2020-04-16 14:00:00"},
+        { id: 4, valveGroup: "V3301", measure: undefined, date: "2020-04-16 14:00:00"}
     ]
-
+    
     const columns = [
         { dataField: 'id', text: '#' },
         { dataField: 'valveGroup', text: 'Valve Name', filter: textFilter() },
         { dataField: 'measure', text: 'Measure', filter: numberFilter() },
         { dataField: 'date', text: 'Date', filter: dateFilter() }
     ]
+
+    const rowClasses = (row, rowIndex) => {
+        return (row.measure === undefined) ? "resolved-anomaly-row" : "unresolved-anomaly-row"
+    }
 
     return (
         <BootstrapTable
@@ -26,6 +29,8 @@ export default function AlertTable(props) {
             columns={ columns }
             filter={ filterFactory() }
             filterPosition="top"
+            rowClasses={ rowClasses }
+            hover
         />
     )
 }
